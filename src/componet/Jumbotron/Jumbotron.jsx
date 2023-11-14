53e9ceb857fdbf34625d63d3f26c623daa65f14b
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2023-11-14 12:54:53 +0800

    Revisi Jumbotron

--- src/componet/Jumbotron/Jumbotron.jsx
+++ src/componet/Jumbotron/Jumbotron.jsx
@@ -14,13 +14,43 @@ const Jumbotron = () => {
           Selamat Datang di CV. Talongka Jaya
         </h1>
       </div> */}
-      <div className="h-48 sm:h-64 xl:h-80 2xl:h-96">
-        <Carousel className="object-contain" pauseOnHover>
-          <img src={img1} alt="..." />
-          <img src={img2} alt="..." />
-          <img src={img3} alt="..." />
-          <img src={img4} alt="..." />
-          <img src={img5} alt="..." />
+      <div className="h-48 sm:h-64 lg:h-[28rem]">
+        <Carousel pauseOnHover>
+          <a
+            href={img1}
+            target="_blank"
+            rel="noreferrer"
+            className="flex h-full w-full items-center justify-center bg-neutral-300">
+            <img src={img1} alt="..." className="h-full w-full object-fill" />
+          </a>
+          <a
+            href={img2}
+            target="_blank"
+            rel="noreferrer"
+            className="flex h-full w-full items-center justify-center bg-neutral-300">
+            <img src={img2} alt="..." className="h-full w-full object-fill" />
+          </a>
+          <a
+            href={img3}
+            target="_blank"
+            rel="noreferrer"
+            className="flex h-full w-full items-center justify-center bg-neutral-300">
+            <img src={img3} alt="..." className="h-full w-full object-fill" />
+          </a>
+          <a
+            href={img4}
+            target="_blank"
+            rel="noreferrer"
+            className="flex h-full w-full items-center justify-center bg-neutral-300">
+            <img src={img4} alt="..." className="h-full w-full object-fill" />
+          </a>
+          <a
+            href={img5}
+            target="_blank"
+            rel="noreferrer"
+            className="flex h-full w-full items-center justify-center bg-neutral-300">
+            <img src={img5} alt="..." className="h-full w-full object-fill" />
+          </a>
         </Carousel>
       </div>
     </div>
